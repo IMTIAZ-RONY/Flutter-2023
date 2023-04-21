878025bd6ddc463d33e40b0c38cbb6f2e0cadf95
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2023-04-21 17:38:31 +0600

    hello

---
+++ getxpractice/android/app/src/main/kotlin/com/example/getxpractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getxpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
