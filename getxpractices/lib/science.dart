60d0731f1f340748860cbfb9a2d8bc2d114e0b57
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2023-04-24 11:26:17 +0600

    toNamed,toOffNamed,toOffAllNamed

--- getxpractices/lib/science.dart
+++ getxpractices/lib/science.dart
@@ -8,15 +8,23 @@ class Science extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title:Text("Science",style:TextStyle(fontSize:16,color:Colors.red ,fontWeight:FontWeight.bold ,letterSpacing:0.6, ) ,),) ,
+      appBar:AppBar(title:Text("Science",
+        style:TextStyle(fontSize:16,color:Colors.red ,fontWeight:FontWeight.bold ,letterSpacing:0.6, ) ,),) ,
       body:Center(
+      child:Column(
+        children: [
+          Text(''' The world stand on Science.We always benifated by Science''',
+            style:TextStyle(color:Colors.amber,fontSize:16,  ) ,),
+           SizedBox(height:250 ,),
+          ElevatedButton(onPressed: (){
+            Get.toNamed("/");
+            ///Get.offNamed("/");
+            ///Get.offAllNamed("/");
+          }, child:Text ("Return Home Page"),),
 
-        child:ElevatedButton(onPressed: (){
-          Get.toNamed("/");
-          ///Get.offNamed("/");
-          ///Get.offAllNamed("/");
-        }, child:Text ("Return Home Page"),),
+        ],
 
+      ) ,
 
       ) ,
 
