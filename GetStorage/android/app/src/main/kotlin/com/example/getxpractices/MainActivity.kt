f60dbf0cb86af1cb0fa32d0803539472c4fad691
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2023-04-24 18:43:24 +0600

    data write,read,remove/erase by getx

---
+++ GetStorage/android/app/src/main/kotlin/com/example/getxpractices/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getxpractices
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ GetStorage/lib/science.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get_storage/get_storage.dart';
+//import 'package:get/get.dart';
+class Science extends StatelessWidget {
+  const Science({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title:Text("Science",
+        style:TextStyle(fontSize:16,color:Colors.red ,fontWeight:FontWeight.bold ,letterSpacing:0.6, ) ,),) ,
+      body:Center(
+
+
+      ) ,
+
+
+    
+    );
+  }
+}
